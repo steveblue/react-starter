6ffa3c3f960fc5595fa2ffa17a8405b4dea85523
Steve Belovarich <steveblue@gmail.com>
2020-01-18 22:57:07 -0800

    FIXED imports

--- src/client/app/view/page.jsx
+++ src/client/app/view/page.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Helmet from 'react-helmet';
+import Helmet from 'react-helmet-async';
 import { Link } from 'react-router-dom';
 
 export default function Page() {
