623e7da915d41f2b9697fca211072e1fc4b08046
Steve Belovarich <steveblue@gmail.com>
2020-01-18 23:05:39 -0800

    FIXED imports again

--- src/client/app/view/root.jsx
+++ src/client/app/view/root.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Helmet from 'react-helmet-async';
+import Helmet from 'react-helmet';
 import { Link } from 'react-router-dom';
 
 export default function Root() {
